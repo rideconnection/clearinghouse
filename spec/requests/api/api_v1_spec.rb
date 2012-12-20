f7d897e52491c50f517f542083018abd0d165102
Chris Bloom <chrisbloom7@gmail.com>
2012-12-20 15:53:28 -0500

    Adding tests to ensure only users belonging to the requesting provider can be accessed from the API

--- spec/requests/api/api_v1_spec.rb
+++ spec/requests/api/api_v1_spec.rb
@@ -34,13 +34,11 @@ describe Clearinghouse::API_v1 do
   context "users" do
     before do
       @user1 = FactoryGirl.create(:user, name: "Phil", active: true, provider: @provider)
+      @user2 = FactoryGirl.create(:user, name: "Bill", provider: @provider)
+      @user3 = FactoryGirl.create(:user, name: "Mark", provider: FactoryGirl.create(:provider))
     end
     
     describe "GET /api/v1/users" do
-      before do
-        @user2 = FactoryGirl.create(:user, name: "Bill", provider: @provider)
-      end
-
       include_examples "requires authenticatable params"
 
       it "should return all provider users as JSON" do
@@ -48,6 +46,7 @@ describe Clearinghouse::API_v1 do
         response.status.should == 200
         response.body.should include(%Q{"name":"#{@user1.name}"})
         response.body.should include(%Q{"name":"#{@user2.name}"})
+        response.body.should_not include(%Q{"name":"#{@user3.name}"})
       end
     end
 
@@ -58,6 +57,12 @@ describe Clearinghouse::API_v1 do
         get "/api/v1/users/show", ApiParamFactory.authenticatable_params(@provider, {:id => @user1.id})
         response.status.should == 200
         response.body.should include(%Q{"name":"#{@user1.name}"})
+      end
+
+      it "should not allow me to access a user belonging to another provider" do
+        get "/api/v1/users/show", ApiParamFactory.authenticatable_params(@provider, {:id => @user3.id})
+        response.status.should == 404
+        response.body.should_not include(%Q{"name":"#{@user3.name}"})
       end
     end
 
@@ -71,11 +76,17 @@ describe Clearinghouse::API_v1 do
         @user1.reload
         @user1.name.should eq("Mary")
       end
+
+      it "should not allow me to access a user belonging to another provider" do
+        put "/api/v1/users/update", ApiParamFactory.authenticatable_params(@provider, {:id => @user3.id, :user => {:name => "Mary"}})
+        response.status.should == 404
+        response.body.should_not include(%Q{"name":"Mary"})
+      end
     end
 
     describe "PUT /api/v1/users/activate" do
       before do
-        @user1 = FactoryGirl.create(:user, name: "Phil", active: false, provider: @provider)
+        @user1.update_attribute(:active, false)
       end
       
       include_examples "requires authenticatable params", :id => 1
@@ -87,11 +98,17 @@ describe Clearinghouse::API_v1 do
         @user1.reload
         @user1.active.should be_true
       end
+
+      it "should not allow me to access a user belonging to another provider" do
+        put "/api/v1/users/activate", ApiParamFactory.authenticatable_params(@provider, {:id => @user3.id})
+        response.status.should == 404
+        response.body.should_not include(%Q{"active":true"})
+      end
     end
 
     describe "PUT /api/v1/users/deactivate" do      
       before do
-        @user1 = FactoryGirl.create(:user, name: "Phil", active: true, provider: @provider)
+        @user1.update_attribute(:active, true)
       end
       
       include_examples "requires authenticatable params", :id => 1
@@ -102,6 +119,12 @@ describe Clearinghouse::API_v1 do
         response.body.should include(%Q{"active":false})
         @user1.reload
         @user1.active.should be_false
+      end
+
+      it "should not allow me to access a user belonging to another provider" do
+        put "/api/v1/users/deactivate", ApiParamFactory.authenticatable_params(@provider, {:id => @user3.id})
+        response.status.should == 404
+        response.body.should_not include(%Q{"active":false"})
       end
     end
   end
