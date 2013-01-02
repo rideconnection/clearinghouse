f55772302a478d7fc693a34433a2a28bba3094b6
Mike Leone <michael.leone@panopticdev.com>
2013-01-02 18:39:50 -0500

    Don't use external SMTP service for staging/production servers.

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Clearinghouse::Application.initialize!
-
-Clearinghouse::Application.configure do
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'rideconnection.org',
-    :user_name            => 'panopticdev',
-    :password             => 'panopticpilot2299',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true,
-    :openssl_verify_mode  => 'none'
-  }
-end
--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,3 +1,5 @@
 # Email setup and constant definitions
 
 EMAIL_FROM = "apps@rideconnection.org"
+
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
