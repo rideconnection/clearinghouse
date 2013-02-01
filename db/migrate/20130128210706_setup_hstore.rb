0c6baaf612d63b950b50fa872e1ca49b1dbdfde7
Mike Leone <michael.leone@panopticdev.com>
2013-02-01 00:06:46 -0500

    Fix old migration for postgres 8.4 environments (staging).

--- db/migrate/20130128210706_setup_hstore.rb
+++ db/migrate/20130128210706_setup_hstore.rb
@@ -1,9 +1,13 @@
 class SetupHstore < ActiveRecord::Migration
+
+  # Removing original migration code since (a) the extension should already be 
+  # activated and (b) the "CREATE EXTENSION" is not supported in Postgres 8.4 environments.
+
   def self.up
-    execute "CREATE EXTENSION IF NOT EXISTS hstore"
+    #execute "CREATE EXTENSION IF NOT EXISTS hstore"
   end
 
   def self.down
-    execute "DROP EXTENSION IF EXISTS hstore"
+    #execute "DROP EXTENSION IF EXISTS hstore"
   end
 end
