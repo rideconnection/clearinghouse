9e3e5b7ec5da9dbf527526679400267eb15c5c56
Chris Bloom <chrisbloom7@gmail.com>
2013-03-01 10:10:54 -0500

    Fixing issue with asset paths on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,4 +68,6 @@ Clearinghouse::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.relative_url_root = '/clearinghouse'
 end
