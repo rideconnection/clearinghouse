f7ade9a388fc7306bc7ce4d78836c63800db20cc
mtjhax <mtj@cmpj.org>
2013-04-02 18:20:43 -0400

    ported rspec unit tests to minitest and removed spec files

--- test/unit/provider_test.rb
+++ test/unit/provider_test.rb
@@ -23,5 +23,113 @@ class ProviderTest < ActiveSupport::TestCase
       @provider.can_auto_approve_for?(provider_3).must_equal false
       provider_3.can_auto_approve_for?(@provider).must_equal false
     end
+
+    it "should include a regenerate_keys! method" do
+      @provider.must_respond_to(:regenerate_keys!)
+    end
   end
+
+  describe "API key" do
+    it "should not be required on new records" do
+      assert_attribute_not_required(FactoryGirl.build(:provider), :api_key)
+    end
+
+    it "should be required on existing records" do
+      assert_attribute_required(@provider, :api_key)
+    end
+
+    it "should be generated automagically for new records when no api_key has been set" do
+      provider = FactoryGirl.build(:provider, :api_key => nil)
+      provider.save
+      provider.reload
+      provider.api_key.wont_be_nil
+    end
+
+    it "should not be generated automagically for new records when an api_key *has* been set" do
+      provider = FactoryGirl.build(:provider, :api_key => "somekey")
+      provider.save
+      provider.reload
+      provider.api_key.must_equal "somekey"
+    end
+
+    it "should be able to be regenerated" do
+      old_api_key = @provider.api_key
+      @provider.regenerate_keys!
+      @provider.reload
+      @provider.api_key.wont_equal old_api_key
+    end
+
+    it "must be unique" do
+      provider = FactoryGirl.build(:provider, :api_key => @provider.api_key)
+      provider.valid?.must_equal false
+      provider.errors.keys.must_include :api_key
+    end
+  end
+
+  describe "private key" do
+    it "should not be required on new records" do
+      provider = FactoryGirl.build(:provider, :private_key => nil)
+      provider.valid?.must_equal true
+    end
+
+    it "should be required on existing records" do
+      @provider.private_key = nil
+      @provider.valid?.must_equal false
+      @provider.errors.keys.must_include :private_key
+    end
+
+    it "should be generated automagically for new records when no private_key has been set" do
+      provider = FactoryGirl.build(:provider, :private_key => nil)
+      provider.save
+      provider.reload
+      provider.private_key.wont_be_nil
+    end
+
+    it "should not be generated automagically for new records when an private_key *has* been set" do
+      provider = FactoryGirl.build(:provider, :private_key => "somekey")
+      provider.save
+      provider.reload
+      provider.private_key.must_equal "somekey"
+    end
+
+    it "should be able to be regenerated" do
+      old_private_key = @provider.private_key
+      @provider.regenerate_keys!
+      @provider.reload
+      @provider.private_key.wont_equal old_private_key
+    end
+  end
+
+
+  describe "with an unapproved partnership" do
+    before do
+      @partner = FactoryGirl.create(:provider)
+      @partnership = ProviderRelationship.create!(
+        :requesting_provider => @provider,
+        :cooperating_provider => @partner
+      )
+    end
+
+    it "should know its approved partnerships" do
+      assert @provider.approved_partnerships.empty?
+      @partnership.approve!
+      assert @provider.approved_partnerships.any?
+    end
+
+    it "should know which partnerships are pending approval and by whom" do
+      assert_equal @provider.pending_partnerships_initiated_by_it, [@partnership]
+      assert_equal @provider.partnerships_awaiting_its_approval, []
+
+      assert_equal @partner.partnerships_awaiting_its_approval, [@partnership]
+      assert_equal @partner.pending_partnerships_initiated_by_it, []
+    end
+  end
+
+  it "should have an association to many nonces" do
+    @provider.must_respond_to(:nonces)
+    nonce = FactoryGirl.build(:nonce)
+    @provider.nonces << nonce
+    @provider.nonces.must_equal [nonce]
+  end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Some test still exist in spec/models/user_spec.rb
-  
+
   it "returns display_name based on available information" do
     user = User.new email: "new_guy@rideconnection.org", name: nil
     
@@ -12,30 +11,52 @@ class UserTest < ActiveSupport::TestCase
     user.name = "Old Guy"
     assert_equal "Old Guy", user.display_name
   end
-  
+
+  describe '#new' do
+    it "is active by default" do
+      user = User.new
+      user.active.must_equal true
+      user.active_for_authentication?.must_equal true
+    end
+  end
+
   describe "validations" do
     setup do
       @user = FactoryGirl.create(:user)
       @provider = FactoryGirl.create(:provider)
       @role = FactoryGirl.create(:role)
     end
-    
-    it "should require a provider" do  
-      @user.provider = nil
-      @user.valid?.must_equal false
-      @user.errors[:provider].must_include "can't be blank"
-      
-      @user.provider = @provider
-      @user.valid?.must_equal true
+
+    it "should require a provider" do
+      assert_attribute_required(@user, :provider, @provider)
     end
   
     it "should require a role" do
-      @user.role = nil
-      @user.valid?.must_equal false
-      @user.errors[:role].must_include "can't be blank"
-      
-      @user.role = @role
-      @user.valid?.must_equal true
+      assert_attribute_required(@user, :role, @role)
+    end
+
+    it "should require an email" do
+      assert_attribute_required(@user, :email, "a@b.c")
+    end
+
+    describe "password" do
+      it "requires a complex password" do
+        # must be 6 - 20 characters in length and have at least one number and at least one non-alphanumeric character
+        assert_does_not_accept_values(@user, :password, "aaaaaa")
+        assert_does_not_accept_values(@user, :password, "aaa123")
+        assert_does_not_accept_values(@user, :password, "aa  aa")
+        assert_does_not_accept_values(@user, :password, "1---1")
+        assert_does_not_accept_values(@user, :password, "aa 12")
+        assert_does_not_accept_values(@user, :password, "aaaaaaaaaaaaaaaaaaa 1")
+
+        assert_accepts_values(@user, :password, "aaaa 1")
+        assert_accepts_values(@user, :password, "aa_123")
+        assert_accepts_values(@user, :password, "1----1")
+        assert_accepts_values(@user, :password, "aaa 12")
+        assert_accepts_values(@user, :password, "11111111111111      ")
+        assert_accepts_values(@user, :password, "aaaaaaaaaaaaaaaaaa 1")
+      end
     end
   end
+
 end
