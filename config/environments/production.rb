12dc6825705ef62a797701db4831c8ef9ff9c65b
Mike Leone <michael.leone@panopticdev.com>
2013-04-10 14:14:49 -0400

    Add actionmailer root URL for outgoing emails. #1423.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,10 @@ Clearinghouse::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.action_mailer.default_url_options = {
+    :host => 'ch.rideconnection.org',
+  }
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
