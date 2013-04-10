8d66f74c1954efbbf7ba6df9c50abcd061823cb1
Mike Leone <michael.leone@panopticdev.com>
2013-04-10 09:39:12 -0400

    Just copy database.yml; don't use shared version for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,11 @@ namespace :deploy do
   end
 end
 
+task :copy_database_yml do
+  puts "    Copy database.yml file"
+  run  "cp #{latest_release}/config/database.yml.example #{latest_release}/config/database.yml"
+end
+
 task :link_database_yml do
   puts "    Link in database.yml file"
   run  "ln -nfs #{deploy_to}/shared/config/database.yml #{latest_release}/config/database.yml"
@@ -29,4 +34,4 @@ task :link_database_yml do
   run  "ln -nfs #{deploy_to}/shared/config/app_config.yml #{latest_release}/config/app_config.yml"
 end
 
-before "deploy:assets:precompile", :link_database_yml
+before "deploy:assets:precompile", :copy_database_yml
