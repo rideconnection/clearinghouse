5c2aff70fd42ffdb3e6d32fdc9ec0cfebeeff45d
Mike Leone <michael.leone@panopticdev.com>
2013-07-01 13:15:15 -0400

    Update DB schema (may change again).

--- db/schema.rb
+++ db/schema.rb
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(:version => 20130624182706) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.datetime "created_at",                                          :null => false
-    t.datetime "updated_at",                                          :null => false
-    t.spatial  "position",   :limit => {:srid=>4326, :type=>"point"}
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.spatial  "position",   :limit => {:no_constraints=>true}
   end
 
   create_table "nonces", :force => true do |t|
@@ -151,12 +151,12 @@ ActiveRecord::Schema.define(:version => 20130624182706) do
     t.string   "name"
     t.integer  "provider_id"
     t.integer  "funding_source_id"
-    t.datetime "created_at",                                                    :null => false
-    t.datetime "updated_at",                                                    :null => false
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
     t.integer  "operating_hours_id"
     t.text     "rate"
     t.hstore   "eligibility"
-    t.spatial  "service_area",       :limit => {:srid=>4326, :type=>"polygon"}
+    t.spatial  "service_area",       :limit => {:no_constraints=>true}
   end
 
   add_index "services", ["provider_id"], :name => "index_services_on_provider_id"
@@ -224,6 +224,7 @@ ActiveRecord::Schema.define(:version => 20130624182706) do
   add_index "trip_ticket_comments", ["trip_ticket_id"], :name => "index_trip_ticket_comments_on_trip_ticket_id"
 
   create_table "trip_tickets", :force => true do |t|
+    t.integer       "origin_provider_id"
     t.string        "origin_customer_id"
     t.integer       "claimant_provider_id"
     t.boolean       "customer_information_withheld"
@@ -269,7 +270,6 @@ ActiveRecord::Schema.define(:version => 20130624182706) do
     t.datetime      "appointment_time"
     t.integer_array "provider_white_list"
     t.integer_array "provider_black_list"
-    t.integer       "origin_provider_id"
     t.boolean       "rescinded",                                           :default => false, :null => false
   end
 
@@ -280,6 +280,7 @@ ActiveRecord::Schema.define(:version => 20130624182706) do
   add_index "trip_tickets", ["customer_service_animals"], :name => "customer_service_animals"
   add_index "trip_tickets", ["guest_or_attendant_assistive_devices"], :name => "guest_or_attendant_assistive_devices"
   add_index "trip_tickets", ["guest_or_attendant_service_animals"], :name => "guest_or_attendant_service_animals"
+  add_index "trip_tickets", ["origin_provider_id"], :name => "index_trip_tickets_on_origin_provider_id"
   add_index "trip_tickets", ["provider_black_list"], :name => "provider_black_list"
   add_index "trip_tickets", ["provider_white_list"], :name => "provider_white_list"
   add_index "trip_tickets", ["trip_funders"], :name => "trip_funders"
