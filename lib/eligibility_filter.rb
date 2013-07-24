99b7b89767efd3946edb32d93f874d86b27ee813
mtjhax <mtj@cmpj.org>
2013-07-24 17:39:06 -0400

    reconstructed service area filter to avoid redundant query expressions

--- lib/eligibility_filter.rb
+++ lib/eligibility_filter.rb
@@ -5,21 +5,12 @@ module EligibilityFilter
     collection ||= TripTicket.all
     query_str = ""
     query_params = []
-    service_area_query = ""
-    service_area_ignore = false
+    service_area_filters = [ :pickup, :dropoff ]
 
     provider.services.each do |service|
       # Service area filtering
-      unless service_area_ignore
-        new_sql = service_area_filter(service, provider)
-        if new_sql.present?
-          service_area_query << " OR " if service_area_query.present?
-          service_area_query << new_sql
-        else
-          # this service ignores service area, so don't filter on service area at all
-          service_area_ignore = true
-        end
-      end
+      service_area_filters = service_area_filters - service_area_unfiltered_location_types(service)
+
       # Eligibility requirements filtering
       service.eligibility_requirements.each do |requirement|
         new_sql, new_params = eligibility_requirement_filter(requirement)
@@ -30,31 +21,55 @@ module EligibilityFilter
         end
       end
     end
-    collection = add_service_area_filter(collection, service_area_query) unless service_area_ignore
+
+    if service_area_filters.present?
+      service_area_query = service_area_filter(service_area_filters, provider)
+      collection = add_service_area_filter(collection, service_area_query)
+    end
+
     collection = collection.where(query_str, *query_params) if query_str.present?
     collection
   end
 
-  def service_area_filter(service, provider)
-    pickup_query = '"((pickup"."position" IS NULL) OR EXISTS (' +
+  # given a service's setting for filtering trip tickets by service area, this returns
+  # an array of location types that will not be filtered at all by this service.
+  # E.g. if a service specifies that only pickup must be within the service area, then it accepts ALL drop-off
+  # locations, so we should not filter by drop-off location at all.
+  def service_area_unfiltered_location_types(service)
+    case service.service_area_type.to_s
+      when 'pickup'
+        [ :dropoff ]
+      when 'dropoff'
+        [ :pickup ]
+      when 'both'
+        []
+      else
+        # this service does not filter by location, it accepts trips that are anywhere
+        [ :pickup, :dropoff ]
+    end
+  end
+
+  def service_area_filter(service_area_filters, provider)
+    # add this at most once to the complete query
+    pickup_query = '(("pickup"."position" IS NULL) OR EXISTS (' +
       'SELECT 1 FROM "services" ' +
         'WHERE (("services"."provider_id" = ' + provider.id.to_s + ') ' +
           'AND ST_Contains("services"."service_area", "pickup"."position"))))'
 
+    # add this at most once to the complete query
     dropoff_query = '(("dropoff"."position" IS NULL) OR EXISTS (' +
       'SELECT 1 FROM "services" ' +
         'WHERE (("services"."provider_id" = ' + provider.id.to_s + ') ' +
           'AND ST_Contains("services"."service_area", "dropoff"."position"))))'
 
-    query = case service.service_area_type.to_s
-      when 'pickup'
-        pickup_query
-      when 'dropoff'
-        dropoff_query
-      when 'both'
-        "(#{pickup_query} AND #{dropoff_query})"
-      else
-        nil
+    if service_area_filters.include?(:pickup) && service_area_filters.include?(:dropoff)
+      "(#{pickup_query} AND #{dropoff_query})"
+    elsif service_area_filters.include?(:pickup)
+      pickup_query
+    elsif service_area_filters.include?(:dropoff)
+      dropoff_query
+    else
+      nil
     end
   end
 
