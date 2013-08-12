5707994f18b797b0c10043ea5e64dddd030de3dd
Chris Bloom <chrisbloom7@gmail.com>
2013-08-12 15:48:41 -0400

    Adding output logging to Whenever scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,8 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
+set :output, "#{path}/log/cron.log"
+
 every '0 * * * 1-5', :roles => [:app] do
   rake 'clearinghouse:trip_tickets:expire'
 end
