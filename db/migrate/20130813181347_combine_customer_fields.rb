da98b53a9981d955930e68ee70e74cfbc1bf3020
mtjhax <mtj@cmpj.org>
2013-08-13 15:19:13 -0400

    Issue #1611 and #1637, migration fixes

--- db/migrate/20130813181347_combine_customer_fields.rb
+++ db/migrate/20130813181347_combine_customer_fields.rb
@@ -5,18 +5,18 @@ class CombineCustomerFields < ActiveRecord::Migration
     execute "UPDATE trip_tickets SET customer_assistive_devices = customer_assistive_devices || guest_or_attendant_assistive_devices"
 
     # drop guest arrays and related indexes
-    remove_index :trip_tickets, :guest_or_attendant_service_animals
-    remove_index :trip_tickets, :guest_or_attendant_assistive_devices
+    remove_index :trip_tickets, name: :guest_or_attendant_service_animals
+    remove_index :trip_tickets, name: :guest_or_attendant_assistive_devices
     remove_column :trip_tickets, :guest_or_attendant_service_animals
     remove_column :trip_tickets, :guest_or_attendant_assistive_devices
 
     # rename customer_assistive_devices column per task #1637
-    remove_index :trip_tickets, :customer_assistive_devices
+    remove_index :trip_tickets, name: :customer_assistive_devices
     rename_column :trip_tickets, :customer_assistive_devices, :customer_mobility_factors
     execute "CREATE INDEX customer_mobility_factors ON trip_tickets USING GIN(customer_mobility_factors)"
 
     # remove eligibility rules that use guest_or_attendant_* (irreversible)
-    execute "DELETE FROM eligibility_rules WHERE trip_field IN ('guest_or_attendant_service_animals', guest_or_attendant_assistive_devices')"
+    execute "DELETE FROM eligibility_rules WHERE trip_field IN ('guest_or_attendant_service_animals', 'guest_or_attendant_assistive_devices')"
 
     # update eligibility rules that use customer_assistive_devices
     execute "UPDATE eligibility_rules SET trip_field = 'customer_mobility_factors' WHERE trip_field = 'customer_assistive_devices'"
@@ -25,7 +25,7 @@ class CombineCustomerFields < ActiveRecord::Migration
   def down
     execute "UPDATE eligibility_rules SET trip_field = 'customer_assistive_devices' WHERE trip_field = 'customer_mobility_factors'"
 
-    remove_index :trip_tickets, :customer_mobility_factors
+    remove_index :trip_tickets, name: :customer_mobility_factors
     rename_column :trip_tickets, :customer_mobility_factors, :customer_assistive_devices
     execute "CREATE INDEX customer_assistive_devices ON trip_tickets USING GIN(customer_assistive_devices)"
 
