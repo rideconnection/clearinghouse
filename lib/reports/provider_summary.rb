81c855939e0da360eab96d71d4ad2fa12d958a5b
mtjhax <mtj@cmpj.org>
2013-09-12 21:31:48 -0400

    first draft of summary report

--- lib/reports/provider_summary.rb
+++ lib/reports/provider_summary.rb
@@ -2,14 +2,17 @@ require 'reports/report'
 
 module Reports
   class ProviderSummary < Report
+    attr_accessor :summary
+
     def initialize(user, options = {})
       @report_user = user
-      @data = []
 
       created_trips = @report_user.provider.trip_tickets.where(date_condition('trip_tickets.created_at', options))
       updated_trips = @report_user.provider.trip_tickets.where(date_condition('trip_tickets.updated_at', options))
-      offers = @report_user.provider.trip_tickets.joins(:trip_claims).where(date_condition('trip_claims.created_at', options))
-      requests = @report_user.provider.trip_claims.where(date_condition('trip_claims.created_at', options))
+      created_offers = @report_user.provider.trip_tickets.joins(:trip_claims).where(date_condition('trip_claims.created_at', options))
+      updated_offers = @report_user.provider.trip_tickets.joins(:trip_claims).where(date_condition('trip_claims.updated_at', options))
+      created_requests = @report_user.provider.trip_claims.where(date_condition('trip_claims.created_at', options))
+      updated_requests = @report_user.provider.trip_claims.where(date_condition('trip_claims.updated_at', options))
 
       section_data = {
         "New trips submitted" => created_trips.count
@@ -22,14 +25,24 @@ module Reports
       create_summary_section("Trip Tickets", section_data)
 
       section_data = {
-        "Claim offers received" => offers.count,
-        "Claim requests made" => requests.count
+        "Total offers received" => created_offers.count
       }
-      create_summary_section("Trip Claims", section_data)
-    end
+      updated_offers.each do |claim|
+        status = claim.status.capitalize
+        section_data[status] ||= 0
+        section_data[status] += 1
+      end
+      create_summary_section("Claim Offers Received", section_data)
 
-    def summary
-      @data
+      section_data = {
+        "Total claim requests" => created_requests.count
+      }
+      updated_requests.each do |claim|
+        status = claim.status.capitalize
+        section_data[status] ||= 0
+        section_data[status] += 1
+      end
+      create_summary_section("Claim Requests Submitted", section_data)
     end
   end
 end
