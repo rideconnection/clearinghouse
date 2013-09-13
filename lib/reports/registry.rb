8ecbfbeebcd2bea463ea82fcd928e2a8fffae669
mtjhax <mtj@cmpj.org>
2013-09-13 15:05:17 -0400

    made sure multiple includes of Reports::Registry class do not generate report list twice

--- lib/reports/registry.rb
+++ lib/reports/registry.rb
@@ -5,18 +5,19 @@ module Reports
     mattr_accessor :report_list
 
     included do
-      REPORTS_PARENT_DIR = File.join(Rails.root, 'lib')
-      Dir[File.join(REPORTS_PARENT_DIR, 'reports', '**', '*_report.rb')].each do |file|
-        require File.join('reports', File.basename(file))
+      if Reports::Registry.report_list.nil?
+        REPORTS_PARENT_DIR = File.join(Rails.root, 'lib')
+        Dir[File.join(REPORTS_PARENT_DIR, 'reports', '**', '*_report.rb')].each do |file|
+          require File.join('reports', File.basename(file))
+        end
+        Reports::Registry.report_list ||= []
+        Reports::Report.descendants.each do |report_class|
+          report_id = report_class.name.rpartition('::').last.underscore
+          report_title = report_class.title if report_class.respond_to?(:title)
+          report_title ||= report_id.titleize
+          Reports::Registry.report_list << { id: report_id, title: report_title, klass: report_class }
+        end
       end
-      Reports::Registry.report_list ||= []
-      Reports::Report.descendants.each do |report_class|
-        report_id = report_class.name.rpartition('::').last.underscore
-        report_title = report_class.title if report_class.respond_to?(:title)
-        report_title ||= report_id.titleize
-        Reports::Registry.report_list << { id: report_id, title: report_title, klass: report_class }
-      end
-
       helper_method :report_title, :report_class, :available_reports
     end
 
