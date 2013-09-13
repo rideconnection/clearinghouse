7c239a14a952fbcd56f9f45cdc49e3a3909f419a
mtjhax <mtj@cmpj.org>
2013-09-12 20:27:03 -0400

    documented report class

--- lib/reports/report.rb
+++ lib/reports/report.rb
@@ -1,3 +1,45 @@
+# Using the Reports::Report class
+#
+# To create a report, first subclass Reports::Report.
+# If you want a table in your report, define the methods #headers and/or #rows.
+# To add a summary section to the report, define the method #summary
+# (Summary sections look like a form, with a label and value on each row).
+# You can use a table or summary section alone, or combine both. Summary appears below the table.
+#
+# Method #headers
+#
+# Should return an array of arrays containing header row values.
+# These will be put into the thead section of the report table.
+#
+# Method #rows
+#
+# Should return an array of rows, where each data row is an array of values.
+# Rows should have the same number of values as the headers. Sparse rows are not supported.
+# Section break rows can be inserted by returning a hash instead of an array as follows:
+# { 'Section Title' => :title }
+# Subtotals and Totals rows can be inserted by returning a hash instead of an array as follows:
+# { [ 1, 2, 3, 4 ] => :subtotal }
+# { [ 10, 20, 30, 40 ] => :total }
+#
+# Method #summary
+#
+# Should return summary data as a hash, e.g.: { "Total widgets" => "215", "Total sprockets" => "305" }
+# If #summary returns an array of hashes, each hash will be displayed as a separate section.
+# To give a section a title, include a hash entry with value :title, e.g. { 'Section Title' => :title }
+#
+# Convenience Methods
+# The following methods can be used to help in generating output data in the proper format:
+#
+# #create_table_section(title, new_rows)
+# #create_title_row(title)
+# #create_data_row(row_values)
+# #create_subtotals_row(row_values)
+# #create_totals_row(row_values)
+# #create_summary_section(title, data_hash)
+#
+# This #date_condition(field_name, options) method can be used to generate a SQL WHERE condition on the
+# specified date or timestamp field. The options hash is assumed to include :date_begin and/or :date_end.
+
 require 'reports/helpers'
 
 module Reports
@@ -19,27 +61,14 @@ module Reports
 
     # TODO might be helpful if report headers and rows can be array of hashes so values can be sparse and in any order
 
-    # if a report has header rows, it should define method #headers
-    # #headers should return an array of arrays containing header row values
     def headers
       @report_instance.try(:headers) || []
     end
 
-    # if a report has rows, it should define method #rows
-    # #rows should return an array of rows, where each data row is an array
-    # section breaks can be inserted by returning a hash for a row as follows:
-    # { 'Section Title' => :title }
-    # subtotals and totals can be inserted by returning a hash in the array as follows:
-    # { [ 1, 2, 3, 4 ] => :subtotal }
-    # { [ 10, 20, 30, 40 ] => :total }
     def rows
       @report_instance.try(:rows) || []
     end
 
-    # a summary section is not displayed as a table, but is displayed like a form with labels and values in each row
-    # reports should return summary data as a hash: { "Total widgets" => "215", "Total sprockets" => "305"  }
-    # if report returns an array of hashes, each hash will be displayed as a section
-    # if a hash entry has value :title, it is treated as a section title
     def summary
       @report_instance.try(:summary)
     end
