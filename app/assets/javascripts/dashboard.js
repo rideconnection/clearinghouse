368170a8e6afc5016b584f72c7337ee29ed0f5d7
Chris Bloom <chrisbloom7@gmail.com>
2013-10-30 18:36:43 -0400

    Updating dashboard js manifest file

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,5 @@
 // json2.js is required for IE 6/7 support
 //= require jquery.browser.min
-//= require jquery.cookie
-//= require jquery.serializeJSON.min
 //= require json2
 //= require underscore-min
 //= require backbone-min
