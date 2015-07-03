ffb7e819239c19412c78c5255e96daf7490dcc7f
mtjhax <mtj@cmpj.org>
2015-07-03 11:25:41 -0400

    fixed asset compilation error caused by new Rails 4 asset handling methods

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'vendor', 'asset
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif dashboard.js)
