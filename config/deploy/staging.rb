fc7472070286623be14560bc2eb4ca639b28752f
Kevin Chambers <kc@kevinchambers.com>
2015-08-30 09:21:11 -0700

    Update cap staging script with new server info

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 set :branch, 'master'
 set :rvm_ruby_version, 'ruby-2.2.2@clearinghouse'
-set :passenger_rvm_ruby_version, 'ruby-2.2.2@global'
-set :deploy_to, '/home/deployer/rails/clearinghouse'
-set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/clearinghouse" }
+set :passenger_rvm_ruby_version, 'ruby-2.2.2@passenger'
+set :deploy_to, '/home/deploy/rails/clearinghouse'
+# set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/clearinghouse" }
 
 # capistrano-rails directives
 set :rails_env, 'staging'
@@ -10,4 +10,4 @@ set :assets_roles, [:web, :app]
 set :migration_role, [:db]
 set :conditionally_migrate, true
 
-server 'ridestage.panopticdev.com', roles: [:app, :web, :db], user: 'deployer'
+server 'ch-staging.rideconnection.org', roles: [:app, :web, :db], user: 'deploy'
